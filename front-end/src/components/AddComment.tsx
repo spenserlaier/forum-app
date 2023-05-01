7e7f6e3d8dbbb1e01b15cf66f2a2ec5f2b1b03bb
Spenser Laier <ssl510@nyu.edu>
2023-05-01 01:31:21 -0400

    modified comment logic

--- front-end/src/components/AddComment.tsx
+++ front-end/src/components/AddComment.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../reducers/rootReducer";
-import {useState, ChangeEvent} from "react";
+import { useState, ChangeEvent } from "react";
 import { submitComment } from "../api";
 import InitialCommentObj from "../objects/InitialCommentObj";
 import { Button, TextField } from "@mui/material";
@@ -9,68 +9,67 @@ import postsSlice from "../reducers/postsSlice";
 import store from "../store/store";
 
 interface CommentProps {
-    postId: string;
+  postId: string;
 }
-
 
 const AddComment = (props: CommentProps) => {
-    const requireRefresh = useSelector ((state: RootState) => {
-        return state.posts.requireRefresh;
-    })
-    const userLoggedIn = useSelector((state:RootState) => {
-        return (state.user.loggedIn);
-    })
-    const userToken = useSelector((state:RootState) => {
-        return (state.user.token);
-    })
-    const username = useSelector((state:RootState) => {
-        return (state.user.username);
-    })
-    const [commentText, setCommentText] = useState("default comment text");
-    const handleCommentChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
-        setCommentText(ev.target.value);
-    }
-    const handleSubmit = async () => {
-        const commentObj: InitialCommentObj = {
-            author: username,
-            body: commentText
-        }
-        const res = await submitComment(userToken || "", commentObj, props.postId);
-        console.log(res);
-        //this doesn't feel like a great solution, but i'm not sure what the
-        //alternatives are--currently the fullpost component uses an effect that refreshes
-        //the data when requireRefresh changes
-        store.dispatch(postsSlice.actions.setRequireRefresh(!requireRefresh));
+  const requireRefresh = useSelector((state: RootState) => {
+    return state.posts.requireRefresh;
+  });
+  const userLoggedIn = useSelector((state: RootState) => {
+    return state.user.loggedIn;
+  });
+  const userToken = useSelector((state: RootState) => {
+    return state.user.token;
+  });
+  const username = useSelector((state: RootState) => {
+    return state.user.username;
+  });
+  const [commentText, setCommentText] = useState("default comment text");
+  const handleCommentChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentText(ev.target.value);
+  };
+  const handleSubmit = async () => {
+    const commentObj: InitialCommentObj = {
+      author: username,
+      body: commentText,
+    };
+    const res = await submitComment(userToken || "", commentObj, props.postId);
+    console.log(res);
+    //this doesn't feel like a great solution, but i'm not sure what the
+    //alternatives are--currently the fullpost component uses an effect that refreshes
+    //the data when requireRefresh changes
+    store.dispatch(postsSlice.actions.setRequireRefresh(!requireRefresh));
 
-        return res;
-    }
-    
-    const commentFieldProps = {
-        value: commentText,
-        onChange: handleCommentChange,
-        style: {color: "beige", borderColor: "yellow"}
-    }
-    const commentStyles =  {
-        fieldSet: {borderColor: "beige !important"},
-        input: {
-            borderColor: "beige"
-        }
-    }
+    return res;
+  };
 
-    const loggedInText = 
+  const commentFieldProps = {
+    value: commentText,
+    onChange: handleCommentChange,
+    style: { color: "beige", borderColor: "yellow" },
+  };
+  const commentStyles = {
+    fieldSet: { borderColor: "beige !important" },
+    input: {
+      borderColor: "beige",
+    },
+  };
+
+  const loggedInText = (
     <div>
-        posting a comment as: {username}
-        <TextField inputProps={commentFieldProps} multiline={true} sx={commentStyles}></TextField>
-        <Button onClick = {() => handleSubmit()}> post it!</Button>
+      posting a comment as: {username}
+      <TextField
+        inputProps={commentFieldProps}
+        multiline={true}
+        sx={commentStyles}
+      ></TextField>
+      <Button onClick={() => handleSubmit()}> post it!</Button>
     </div>
-    const notLoggedInText = 
-    <div>
-        log in if you wanna post a comment, man
-    </div>
+  );
+  const notLoggedInText = <div>log in if you wanna post a comment, man</div>;
 
-    return (
-        userLoggedIn? loggedInText : notLoggedInText
-   ) 
-}
+  return userLoggedIn ? loggedInText : notLoggedInText;
+};
 
 export default AddComment;
